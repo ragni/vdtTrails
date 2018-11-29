9d14b34d4b9dcb6c5737df6e503507b9b355491e
abhinav <abhinav.mishra1801@gmail.com>
2018-11-29 23:44:54 +0530

    Added files

--- tests/unit/home/welcome/route-test.js
+++ tests/unit/home/welcome/route-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | login/welcome', function(hooks) {
+module('Unit | Route | home/welcome', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:login/welcome');
+    let route = this.owner.lookup('route:home/welcome');
     assert.ok(route);
   });
 });
