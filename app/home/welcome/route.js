59da24a479fdbc0f886275624576edfe6e9a471b
abhinav <abhinav.mishra1801@gmail.com>
2018-11-29 23:49:47 +0530

    removed unnecessary code

--- app/home/welcome/route.js
+++ app/home/welcome/route.js
@@ -2,12 +2,5 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     function () {
-        console.log(changeset.get('email'));
-        console.log('YEppp');
-        
     },
-
-    model(params) {
-        console.log(params);
-    }
 });
